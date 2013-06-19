bae1a15ce9d225859d7472284f3d9334483a9cb3
Jiří Vysoký <jiri.vysoky@gmail.com>
2013-06-19 14:24:20 +0200

    Updated script

--- use.sh
+++ use.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+
+mvn clean install
+
+echo Installing ANT tool
 generated=ant/target/*-jar-with-dependencies.jar
 target=/home/dropbox/epub-project/lib/antpub.jar
 #target=/home/jirka/.ant/lib/antpub.jar
-mvn clean install
 mvn package assembly:assembly -DdescriptorId=jar-with-dependencies -pl ant
+rm $target
+cp $generated $target
+
+echo Installing epubtool
+generated=cli/target/*-jar-with-dependencies.jar
+target=/home/jirka/bin/epubtool.jar
+mvn package assembly:assembly -DdescriptorId=jar-with-dependencies -pl cli
 rm $target
 cp $generated $target
